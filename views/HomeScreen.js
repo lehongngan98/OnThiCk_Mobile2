04f9ef5aee22e49f0eaa9c88d2ddc6301d3e8d14
Lê Hồng Ngân 20036651 <lehongngan.998@gmail.com>
2024-04-21 14:29:17 +0700

    fist

---
+++ views/HomeScreen.js
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+    <SafeAreaView style={{backgroundColor: "green"}}>
+      <Text>Home</Text>
+    </SafeAreaView>
+    </>
+  )
+}
